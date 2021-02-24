6d81386cc93b57ea4ed46a33674b07d93675828d
araujogabriel77 <araujogabrielocn@gmail.com>
2021-02-24 10:44:07 -0300

    console log na inicializaçao do servidor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+  await app.listen(3333, () => {
+    console.log('Servidor rodando na porta 3333');
+  });
 }
 bootstrap();
